6d07412029331f6165291dc470f08aaed319ca60
mayakovsk1 <artemku.04gt@gmail.com>
2021-03-01 18:33:29 +0300

    First commit

---
+++ main.py
@@ -0,0 +1,38 @@
+import telebot
+import os
+from relevance import entry_relevance
+
+
+TOKEN = os.environ.get('TOKEN')
+bot = telebot.TeleBot(TOKEN, parse_mode=None)
+
+
+string1 = '''Зравствуйте, вас приветствует служба поддержки Ростелекома.
+Пожалуйста введите ваш запрос...'''
+
+
+string2 = '''Пожалуйста введите ваш запрос...'''
+
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+	bot.send_message(message.chat.id, string1)
+
+
+@bot.message_handler(commands=['help'])
+def send_welcome(message):
+	bot.send_message(message.chat.id, string2)
+
+
+@bot.message_handler(content_types=['text'])
+def send_answer(message):
+	bot.send_message(message.chat.id, '''Ожидайте, ваш запрос обрабатывается...''')
+	answer = entry_relevance(str(message))
+	bot.send_message(message.chat.id, answer)
+	bot.send_message(message.chat.id, '''Пожалуйста введите ваш запрос...''')
+	# print('<<', message, '>>')
+	# print('{{', answer, '}}')
+	# print()
+
+
+bot.polling()
---
+++ relevance.py
@@ -0,0 +1,40 @@
+from keywords import key_words
+from dictionary import dictionary_key_words
+
+
+#  {релевантность: заголовок}
+relevance_check = {}
+string = '''Извините, не можем ответить на ваш вопрос.
+Пожалуйста, обратитесь в службу поддержки по номеру: 8-800-1000-800'''
+
+
+#  Сравнивает введенные слова с добытыми с сайта
+#  question - введённые слова
+def entry_relevance(question):
+
+
+    try:
+        #  проверка на соответствие слов
+        for i in dictionary_key_words:
+            relevance_check[len(key_words(question) & dictionary_key_words[i])] = i
+        relevance_check_value = relevance_check[max(relevance_check)]
+
+
+        if max(relevance_check) < 1:
+            relevance_check.clear()
+            return string
+        else:
+            relevance_check.clear()
+            return relevance_check_value
+
+
+    except Exception:
+        return string
+
+
+if __name__ == '__main__':
+    while True:
+        message = input()
+        if message == '0':
+            break
+        print(entry_relevance(message))
