c775fde9f0f8d19851d44be2070a995966a160e2
mayakovsk1 <artemku.04gt@gmail.com>
2021-03-05 15:05:15 +0300

    !!!

--- main.py
+++ main.py
@@ -31,8 +31,8 @@ def send_answer(message):
 	bot.send_message(message.chat.id, answer)
 	bot.send_message(message.chat.id, '''Пожалуйста введите ваш запрос...''')
 	print('<<', message, '>>')
-	for i in message:
-		print(i)
+	print()
+	print(message.chat.username)
 	print()
 	print('{{', answer, '}}')
 	print()
